aff40a5d55145f73a74a82287befcfd388e3537e
Mattool81974 <mmattmmo@gmail.com>
2025-10-15 19:00:42 +0200

    Start upgrading polynome system

--- sources/pleos_text.cpp
+++ sources/pleos_text.cpp
@@ -257,7 +257,7 @@ namespace pleos {
 	std::shared_ptr<Tree<std::string>> tree_from_xml(std::shared_ptr<scls::__XML_Text_Base> xml, scls::Text_Style needed_style){std::shared_ptr<Tree<std::string>> tree = std::make_shared<Tree<std::string>>();__tree_add_datas(xml, needed_style, tree.get());return tree;}
 
 	// Generate a word
-	bool is_special_pleos_balise(std::string name){return name == std::string("definition") || name == std::string("graph") || name == std::string("graphic") || name == std::string("linked_list") || name == std::string("table") || name == std::string("theorem") || name == std::string("tree");}
+	bool is_special_pleos_balise(std::string name){return name == std::string("definition") || name == std::string("graph") || name == std::string("graphic") || name == std::string("linked_list") || name == std::string("poly") || name == std::string("table") || name == std::string("theorem") || name == std::string("tree");}
 	std::shared_ptr<scls::__Image_Base> generate_text_image(std::shared_ptr<scls::__XML_Text_Base> current_text, scls::Text_Style needed_style, std::shared_ptr<Text_Environment> possible_environment){return generate_text_image(current_text, needed_style, std::shared_ptr<scls::__XML_Text_Base>(), possible_environment);}
 	std::shared_ptr<scls::__Image_Base> generate_text_image(std::shared_ptr<scls::__XML_Text_Base> current_text, scls::Text_Style needed_style, std::shared_ptr<scls::__XML_Text_Base> parent_text, std::shared_ptr<Text_Environment> possible_environment){
 	    std::string balise_content = current_text.get()->xml_balise();
@@ -312,6 +312,38 @@ namespace pleos {
                 current_text.get()->set_xml_balise_name("span");current_text.get()->set_text(needed_content);
                 utf_8_symbol_xml(current_text, true); // A RE REFLECHIR
             }
+        }
+        else if(current_balise_name == std::string("poly")) {
+            // Handle the attributes
+            std::vector<scls::XML_Attribute>& attributes = current_text.get()->xml_balise_attributes();
+            std::string coefficients = std::string();std::string definition_content_name = std::string();
+            for(int i = 0;i<static_cast<int>(attributes.size());i++) {
+                if(attributes[i].name == std::string("m")){coefficients = attributes[i].value;}
+            }
+
+            // Get the good value
+            std::vector<std::string> cutted = scls::cut_string(coefficients, std::string(";"));
+            std::string result = std::string();
+            for(int i = 0;i<static_cast<int>(cutted.size());i++) {
+                std::vector<std::string> parts = scls::cut_string(cutted.at(i), ":");
+                std::string current_coefficient = std::string("1");
+                if(parts.size() == 1){current_coefficient = parts.at(0);}
+
+                // Asserts
+                bool full_coefficient_digit = true;
+                for(int j = 0;j<static_cast<int>(current_coefficient.size());j++){if(!(std::isdigit(current_coefficient.at(j)) || current_coefficient.at(j) == '.')){full_coefficient_digit=false;break;}}
+                if(current_coefficient == std::string() || (full_coefficient_digit && std::stoi(parts.at(0)) == 0)){continue;}
+                else if(current_coefficient == std::string("1")){current_coefficient = std::string();}
+
+                // Update the result
+                if(result != std::string()){result += std::string("<mo>+</mo>");}
+                if(current_coefficient != std::string()){result += std::string("<mi>") + current_coefficient + std::string("</mi>");}
+            }
+
+            // Set the result
+            current_text.get()->set_xml_balise_name(std::string("math"));
+            current_text.get()->set_text(result);
+            std::cout << "G " << current_text.get()->full_text() << std::endl;
         }
 
         return to_return;
